99d21204a2958d8e60ef66991360c38b742ac213
Mohamed amine Bounya <mobounya@gmail.com>
2024-08-12 00:05:20 +0100

    CLI: add info command
    Add 'info' command as CLI argument to print the torrent metainfo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use crate::metainfo::Parser;
+use std::env;
+use crate::metainfo::{Parser};
 
 mod metainfo;
 
 fn main() {
-    let parser = Parser::new("./sample.torrent".to_string());
-    let meta_info = parser.parse().expect("");
-    println!("{:?}", meta_info);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: cargo run [command] [TORRENT_FILE_PATH]");
+        return;
+    }
+    if args[1] == "info" {
+        let torrent_file_path = args[2].clone();
+        let parser = Parser::new(torrent_file_path.clone());
+        let metainfo = match parser.parse() {
+            Ok(parsed_value) => parsed_value,
+            Err(_) => {
+                eprintln!("Could not decode file: {}", torrent_file_path);
+                return;
+            }
+        };
+        print!("{}", metainfo);
+    }
 }
--- src/metainfo/metainfo.rs
+++ src/metainfo/metainfo.rs
@@ -1,5 +1,7 @@
+use std::fmt::{Display, Formatter};
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
+use sha1::{Digest, Sha1};
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct File {
@@ -23,14 +25,47 @@ pub struct Info {
 // https://wiki.theory.org/BitTorrentSpecification#Metainfo_File_Structure
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub struct TorrentMetaInfo {
-    info : Info,
-    announce : String,
+    pub info : Info,
+    pub announce : String,
     #[serde(rename = "announce-list")]
-    announce_list : Option<Vec<Vec<String>>>,
+    pub announce_list : Option<Vec<Vec<String>>>,
     #[serde(rename = "creation date")]
-    creation_date : Option<i64>,
-    comment : Option<String>,
+    pub creation_date : Option<i64>,
+    pub comment : Option<String>,
     #[serde(rename = "created by")]
-    created_by : Option<String>,
-    encoding : Option<String>,
+    pub created_by : Option<String>,
+    pub encoding : Option<String>,
+}
+
+fn bytes_to_hex_string(bytes: &[u8]) -> String {
+    bytes.iter().map(|b| format!("{:02X}", b)).collect()
+}
+
+impl Display for TorrentMetaInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let hash = Sha1::digest(serde_bencode::to_bytes(&self.info).unwrap());
+        let hex_hash = base16ct::lower::encode_string(&hash);
+        writeln!(f, "Tracker url: {}", self.announce);
+        writeln!(f, "name: {}", self.info.name)?;
+        writeln!(f, "piece length: {}", self.info.piece_length)?;
+        if let Some(length) = self.info.length {
+            writeln!(f, "length: {}", length);
+        } else {
+            writeln!(f, "length: None");
+        }
+        writeln!(f, "info hash: {}", hex_hash);
+        if let Some(md5sum) = self.info.md5sum.clone() {
+            write!(f, "md5sum: {}", md5sum);
+        } else {
+            writeln!(f, "md5sum: None");
+        }
+        writeln!(f, "pieces: ");
+        let mut i = 0;
+        while i < self.info.pieces.len() {
+            let segment = &self.info.pieces[i..i + 20];
+            writeln!(f, "{}", bytes_to_hex_string(segment));
+            i += 20;
+        }
+        write!(f, "")
+    }
 }
